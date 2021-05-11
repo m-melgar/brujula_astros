2f5831df06044291090aee74ab856809b3ac0bb8
mmelgar <marcos.melgar.segovia@gmail.com>
2021-05-11 13:51:00 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy
+
+def cal_astral():
+
+
+    return {'mercury': mercury_coor, 'venus:' venus_coor, 'mars': mars_coor, 'jupyter':jupyter_coor, 'saturn':saturn_coor, 'uranus': uranus_coor, 'moon': moon_coor
+
+
+if __name__ == '__main__':
+
+    while True:
+
+        mercury, venus, mars, jupyter, saturn, uranus, moon = cal_astral()
+
+
+
