37e935a12f7e93f92a2e1961ccfa927580e5a50f
mmelgar <marcos.melgar.segovia@gmail.com>
2021-05-11 14:04:06 +0200

    checkpoint

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import time
 
+
 def cal_astral():
+    """
+
+    :return: dictionary containing astral positions
+    """
 
 
     return {'mercury': mercury_coor, 'venus:' venus_coor, 'mars': mars_coor, 'jupyter':jupyter_coor, 'saturn':saturn_coor, 'uranus': uranus_coor, 'moon': moon_coor
 
+def get_constellation(astra_dict):
+    """
 
+    :param astra_dict: dictionary with astral positions
+    :return: dictionary
+    """
 if __name__ == '__main__':
 
     while True:
