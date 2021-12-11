931d102f4915e193a6157a5aa500757d119e9f03
Marcos Melgar <marcos.melgar.segovia@gmail.com>
2021-12-11 11:17:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,55 @@
 import time
 import config as cfg
 import functions
+import RPi.GPIO as gpio
 from functions import cal_astral, led_trigger, led_trigger_inverted
 from shifter import Shifter
-
 
 if __name__ == '__main__':
 
     # SETUP
-    s1  = Shifter(cfg.SR_1_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
-    s2  = Shifter(cfg.SR_2_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
-    s3  = Shifter(cfg.SR_3_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
-    s4  = Shifter(cfg.SR_4_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
-    s5  = Shifter(cfg.SR_5_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
-    s6  = Shifter(cfg.SR_6_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
-    s7  = Shifter(cfg.SR_7_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
-    s8  = Shifter(cfg.SR_8_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
-    s9  = Shifter(cfg.SR_9_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
-    s10 = Shifter(cfg.SR_10_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
-    s11 = Shifter(cfg.SR_11_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
-    s12 = Shifter(cfg.SR_12_INPUT, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
+#    sf = Shifter(cfg.SR_DB_A, cfg.SR_DB_B,cfg.CLOCK_PIN, cfg.CLEAR_PIN)
+    s1 = Shifter(cfg.SR_1_A, cfg.SR_1_B, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
+    s2 = Shifter(cfg.SR_2_A, cfg.SR_2_B, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
+    s3 = Shifter(cfg.SR_3_A, cfg.SR_3_B, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
+    s4 = Shifter(cfg.SR_4_A, cfg.SR_4_B, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
+    s5 = Shifter(cfg.SR_5_A, cfg.SR_5_B, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
+    s6 = Shifter(cfg.SR_6_A, cfg.SR_6_B, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
+    s7 = Shifter(cfg.SR_7_A, cfg.SR_7_B, cfg.CLOCK_PIN, cfg.CLEAR_PIN)
+    s1.setValue(8)
+    s2.setValue(8)
+    s3.setValue(8)
+    s4.setValue(8)
+    s5.setValue(8)
+    s6.setValue(8)
+    s7.setValue(8)
+    time.sleep(5)
+    s1.clear()
+    s2.clear()
+    s3.clear()
+    s4.clear()
+    s5.clear()
+    s6.clear()
+    s7.clear()
+    s1.setValue(0)
+    s2.setValue(0)
+    s3.setValue(0)
+    s4.setValue(0)
+    s5.setValue(0)
+    s6.setValue(0)
+    s7.setValue(0)
+    time.sleep(5)
+    s1.clear()
+    s2.clear()
+    s3.clear()
+    s4.clear()
+    s5.clear()
+    s6.clear()
+    s7.clear()
 
-    s1.setupBoard()
-    s2.setupBoard()
-    s3.setupBoard()
-    s4.setupBoard()
-    s5.setupBoard()
-    s6.setupBoard()
-    s7.setupBoard()
-    s8.setupBoard()
-    s9.setupBoard()
-    s10.setupBoard()
-    s11.setupBoard()
-    s2.setupBoard()
+    gpio.cleanup()
 
+    """
     # CONSTELLATION
     while True:
 
@@ -53,7 +69,7 @@ if __name__ == '__main__':
 
             AA = functions.data_transform_to_register(led_trigger_inverted(constell_dict_inverted))
             print(AA)
-
+        
         s1.setValue(DATA[0])
         s2.setValue(DATA[1])
         s3.setValue(DATA[2])
@@ -66,7 +82,7 @@ if __name__ == '__main__':
         s10.setValue(DATA[9])
         s11.setValue(DATA[10])
         s12.setValue(DATA[11])
-
+        
         time.sleep(cfg.SLEEP_TIME)
 
         constell_dict_ = cal_astral()
@@ -75,3 +91,5 @@ if __name__ == '__main__':
 
         if DATA_NEW != DATA:
             functions.pretty_transition()
+    """
+   
